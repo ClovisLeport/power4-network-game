f8a3db0764bd02e934529b2f1dbd3cebbcd9b45c
Clovis LEPORT <E228541P@u-inf-j-e114-08.ad.iut-nantes.univ-nantes.prive>
2023-11-15 17:47:23 +0100

    go mod serverp4

--- client/game.go
+++ client/game.go
@@ -1,4 +1,4 @@
-package main
+package client
 
 // Structure de données pour représenter l'état courant du jeu.
 type game struct {
--- client/layout.go
+++ client/layout.go
@@ -1,4 +1,4 @@
-package main
+package client
 
 // Taille de l'affichage en fonction de la taille de la fenêtre.
 func (g *game) Layout(windowWidth, windowHeight int) (screenWidth, screenHeight int) {
