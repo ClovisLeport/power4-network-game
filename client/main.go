fd1d4b0559b578cdcee2dd9e91d354be234ce566
Clovis LEPORT <E228541P@u-inf-j-e113-25.ad.iut-nantes.univ-nantes.prive>
2023-11-30 09:43:52 +0100

    push

--- client/main.go
+++ client/main.go
@@ -1,10 +1,8 @@
 package client
 
 import (
-	"bufio"
 	"log"
 	"net"
-	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
@@ -40,26 +38,6 @@ func init() {
 	offScreenImage = ebiten.NewImage(globalWidth, globalHeight)
 }
 
-func handleClient(c net.Conn) {
-	in := bufio.NewReader(c)
-	out := bufio.NewWriter(c)
-
-	msg := "ping\n"
-	_, err := out.WriteString(msg)
-	if err != nil {
-		log.Fatal("Write error: ", err)
-	}
-	out.Flush()
-	log.Println("Sent: ", msg)
-
-	msg_rcv, err := in.ReadString(byte('\n'))
-	if err != nil {
-		log.Fatal("Read error: ", err)
-	}
-	log.Println("Received", msg_rcv)
-
-}
-
 func handleConnection(c net.Conn) {
 	in_buf := make([]byte, 64, 256)
 	_, err := c.Read(in_buf)
@@ -75,34 +53,15 @@ func handleConnection(c net.Conn) {
 	}
 	log.Println("Sent: ", msg)
 }
-func createServ() {
-	listener, err := net.Listen("tcp", "localhost:8080")
-	if err != nil {
-		log.Println("listen error:", err)
-		return
-	}
-	defer listener.Close()
 
-	conn, err := listener.Accept()
-	if err != nil {
-		log.Println("accept error:", err)
-		return
-	}
-	defer conn.Close()
-	/*log.Println("Le client s'est connecté")*/
-	handleClient(conn)
-
-	time.Sleep(10 * time.Second)
-
-}
 func createClient() {
 	conn, err := net.Dial("tcp", "localhost:8080")
 	if err != nil {
 		log.Println("Dial error:", err)
 		return
 	}
-	defer conn.Close()
 
+	defer conn.Close()
 	log.Println("Je suis connecté")
 	handleConnection(conn)
 }
@@ -114,7 +73,7 @@ func main() {
 
 	ebiten.SetWindowTitle("Programmation système : projet puissance 4")
 	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	createServ()
+
 	createClient()
 	if err := ebiten.RunGame(&g); err != nil {
 		log.Fatal(err)
--- server/main.go
+++ server/main.go
@@ -1,5 +1,33 @@
-package server
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+)
+
+func server() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println("listen error:", err)
+		return
+	}
+	defer listener.Close()
+
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Println("accept error:", err)
+		return
+	}
+
+	defer conn.Close()
+	log.Println("Le client s'est connecté")
+
+	time.Sleep(10 * time.Second)
+
+}
 
 func main() {
+	server()
 
 }
